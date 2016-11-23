6d76e84bffcd55530636b7cc630160c2870a0ad4
Ilya Zabrodsky <ilya.zabrodsky@effective-soft.com>
2016-11-23 16:54:48 +0300

    add create via Drag and Drop .txt

--- src/components/create/todo-create.js
+++ src/components/create/todo-create.js
@@ -5,13 +5,53 @@ angular.module('todoApp.components')
     },
     templateUrl: 'components/create/todo-create.html',
     controller: TodoCreateController
-  });
+  })
+  .directive('drag', [function () {
+    return {
+      restrict: 'EA',
+      scope: {content:'='},
+      link: function (scope, element, attrs, controller) {
 
-function TodoCreateController(ToDoService) {
+        scope.content = "or drop a .txt file here, edit and copy upwards";
+
+        var processDragOverOrEnter;
+
+        processDragOverOrEnter = function (event) {
+          if (event !== null) {
+            event.preventDefault();
+          }
+          event.dataTransfer.effectAllowed = 'copy';
+          return false;
+        };
+
+        element.bind('dragover', processDragOverOrEnter);
+        element.bind('dragenter', processDragOverOrEnter);
+        element.bind('drop', handleDropEvent);
+
+        function insertText(loadedFile) {
+
+          scope.content = loadedFile.target.result;
+          scope.$apply();
+        }
+
+        function handleDropEvent(event) {
+
+          if (event !== null) {
+            event.preventDefault();
+          }
+          var reader = new FileReader();
+          reader.onload = insertText;
+          reader.readAsText(event.dataTransfer.files[0]);
+        }
+      }
+    };
+  }]);
+
+function TodoCreateController($state, ToDoService) {
   var self = this;
-
+  console.log(this.text);
   self.addTodo = function() {
     ToDoService.addTodo(self.todoText);
-    self.todoText = '';
+    $state.go('todos');
   };
 }
